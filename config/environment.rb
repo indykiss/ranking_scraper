b6d28ed5f6b2ab690ae46da117ad8fe0fdad4a67
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-11-05 02:12:50 +0000

    bin/console does running

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require 'nokogiri'
 require 'open-uri'
 require 'capybara'
 require 'capybara/poltergeist'
+
+
+require_relative '../lib/ranking_scraper/scraper'
+require_relative '../lib/ranking_scraper/version'
+require_relative '../lib/ranking_scraper/cli'
+require_relative '../lib/ranking_scraper/ranking'
