4a658124ac3b831fc5574788ac23e162dbcad60f
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-11-05 01:30:19 +0000

    save

--- lib/ranking_scraper/ranking.rb
+++ lib/ranking_scraper/ranking.rb
@@ -3,9 +3,18 @@ class RankingScraper::Ranking
   # I take the info from website and formats it for presenting to user with CLI. 
   
   attr_accessor :company_name, :rank, :country, :profits, :sales
+  @@all = []
   
   def self.new_from_index_page(rank)
-    
+    self.new(
+      rank.css("rank").text, 
+      rank.css("name").text)
+  end 
+  
+  def initialize(rank, name)
+    @rank = rank 
+    @name = name 
+    @@all << self
   end 
   
   
