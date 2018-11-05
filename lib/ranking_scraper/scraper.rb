0b438e61b800892e445490836a3bf49bf97a7b56
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-11-05 00:59:59 +0000

    save

--- lib/ranking_scraper/scraper.rb
+++ lib/ranking_scraper/scraper.rb
@@ -4,7 +4,7 @@ class RankingScraper::Scraper
 
   def connect_page 
     session = Capybara::Session.new(:poltergeist)
-    session.visit('http://stackoverflow.com/')
+    session.visit('https://www.forbes.com/global2000/list/#tab:overall')
     puts session.document.title
   end 
   
