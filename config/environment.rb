ec90b2efd1b23ab15a1b61ec7eb125534a774afb
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-11-07 19:56:28 +0000

    fixed names in config

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,11 @@
-#require 'pry' 
+require 'pry'
 require 'nokogiri'
 require 'open-uri'
 #require 'capybara'
 #require 'capybara/poltergeist'
 
 
-require_relative '../lib/ranking_scraper/scraper'
-require_relative '../lib/ranking_scraper/version'
-require_relative '../lib/ranking_scraper/cli'
-require_relative '../lib/ranking_scraper/ranking'
+require_relative '../lib/ranking_scraper/scraper.rb'
+require_relative '../lib/ranking_scraper/version.rb'
+require_relative '../lib/ranking_scraper/cli.rb'
+require_relative '../lib/ranking_scraper/ranking.rb'
