17a7ef1ad0f58fcdf527053d53fbb0bf3061ca93
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-11-07 20:39:53 +0000

    added code to scraper

--- lib/ranking_scraper/cli.rb
+++ lib/ranking_scraper/cli.rb
@@ -12,15 +12,8 @@ class RankingScraper::CLI
     # input = gets.strip.to_i 
     end 
 
-  def second_step_if_needed
-    # Ok now I'm taking this info that you said and doing a thing 
-    end 
-  
   def print_info 
     # here I give the user the info that they've requested
     end 
   
-  def print_second_info_if_needed
-    # print the second level of info desired 
-   end 
 end 
--- lib/ranking_scraper/scraper.rb
+++ lib/ranking_scraper/scraper.rb
@@ -2,13 +2,17 @@
 
 class RankingScraper::Scraper 
 
-  def connect_page 
-   Nokogiri::HTML(open("http://books.toscrape.com/"))
-  end 
-  
-  def scrape_site 
-    self.get_page.css("div.container-fluid.page")
+  def self.connect_page 
+   book_page = Nokogiri::HTML(open("http://books.toscrape.com/"))
+   
+   book_page.css("div.container-fluid.page").collect do |attribute|
     #could be div.col.sm-8.col-md-9
+    
+    book_list = {
+      :name = attribute.css("h3").text.strip,
+      :product_price = attribute.css("div.product_price").text.strip,
+      :availability = attribute.css("p.instock.availability").text.strip
+    }
   end 
 
   def data_to_info
