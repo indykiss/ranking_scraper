4ebe5107f1355e105340d26c8bbd9ee13cdca072
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-11-08 02:15:31 +0000

    add a bit

--- lib/ranking_scraper/cli.rb
+++ lib/ranking_scraper/cli.rb
@@ -3,7 +3,15 @@ class RankingScraper::CLI
   
   def call 
     puts "Welcome to book scraper."
-    puts "What can I do for you?"
+    puts "Please input the name of the book you're looking for, and we will provide price and availability."
+    puts "If the book is not available, we will let you know."
+    input = gets.strip.to_i
+    
+    if input == #name of a book
+      # output price & availability of book
+      
+    else if input == #none of the books 
+      puts "Sorry, we don't have that book."
   end 
   
   def start 
