5464245e9b6f77f2b583c3c65489a14290fec33e
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-11-11 18:45:24 +0000

    broken bone

--- lib/scraper/cli.rb
+++ lib/scraper/cli.rb
@@ -10,22 +10,24 @@ class CLI
   def fake_question
     puts "Welcome to book scraper."
     puts "Here are the available books: Great Gatsby, Inkheart, Puppies"
+    puts "Please input the name of the book you're looking for, and we will provide price and availability. Or press exit if you'd like to exit."   
   end  
   
   def get_input 
-    puts "Please input the name of the book you're looking for, and we will provide price and availability."   
+    
     input = nil 
     
     while input != "exit"
-    input = gets.strip.downcase
+      input = gets.strip.downcase
     
-    case input 
-      when "Great Gatsby"
-        puts "The Great Gatsby is available and costs $19."
-      when "Inkheart"
-         puts "Inkheart is available and costs $22."
-      when "Puppies"
-         puts "Puppies is not available."
+      case input 
+        when "Great Gatsby"
+          puts "The Great Gatsby is available and costs $19."
+        when "Inkheart"
+           puts "Inkheart is available and costs $22."
+        when "Puppies"
+           puts "Puppies is not available."
+      end 
     end 
   end 
   
@@ -34,15 +36,6 @@ class CLI
     puts "Thanks for coming."
   end 
   
-  
-  def start 
-    # Hi. So here are like 2-3 options maybe. Maybe just one option. 
-    # input = gets.strip.to_i
-    end 
-
-  def print_info 
-    # here I give the user the info that they've requested
-    end 
  
   
 end 
