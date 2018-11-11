181f7976d013b8d0ed6be43174039e49b043ba6c
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-11-11 18:28:39 +0000

    added more bones

--- lib/scraper/cli.rb
+++ lib/scraper/cli.rb
@@ -2,27 +2,35 @@
 class CLI 
   
   def call 
-    puts "Welcome to book scraper."
-  #  fake_list
-    puts "Please input the name of the book you're looking for, and we will provide price and availability."
-  #  fake_response
-    
-    #input = gets.strip.to_i
-      #if input == #name of a book
-        # output price & availability of book
-          #else if input == #none of the books 
-          #  puts "Sorry, we don't have that book."
+    fake_question
+    get_input
+    fake_response
+
   end 
   
-  def fake_list 
-    fake_list = "Great Gatsby, Inkheart, Puppies"
+  def fake_question
+    puts "Welcome to book scraper."
+    puts "Here are the available books: Great Gatsby, Inkheart, Puppies"
+    puts "Please input the name of the book you're looking for, and we will provide price and availability."
   end  
   
+  def get_input 
+    input = gets.strip.to_i
+    
+    case input 
+      when "Great Gatsby"
+        puts "The Great Gatsby is available and costs $19."
+      when "Inkheart"
+         puts "Inkheart is available and costs $22."
+      when "Puppies"
+         puts "Puppies is not available."
+    end 
+  end 
   
   def fake_response 
-    # gets the input but we are building bones now 
-     fake_response = "The Great Gatsby is available and costs $19."
+     puts "The Great Gatsby is available and costs $19."
   end 
+  
   
   def start 
     # Hi. So here are like 2-3 options maybe. Maybe just one option. 
