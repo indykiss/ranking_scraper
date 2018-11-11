5925497f4330cac9a38c79cfaba4579a16e3e038
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-11-11 17:55:22 +0000

    its been like 10 mins

--- lib/scraper/cli.rb
+++ lib/scraper/cli.rb
@@ -3,15 +3,29 @@ class ScraperModule::CLI
   
   def call 
     puts "Welcome to book scraper."
+    fake_list
     puts "Please input the name of the book you're looking for, and we will provide price and availability."
-    puts "If the book is not available, we will let you know."
-    input = gets.strip.to_i
+    
+    fake_response
+    
+    
+    #input = gets.strip.to_i
     
     #if input == #name of a book
       # output price & availability of book
       
     #else if input == #none of the books 
     #  puts "Sorry, we don't have that book."
+  end 
+  
+  def fake_list 
+    fake_list = "Great Gatsby, Inkheart, Puppies"
+  end  
+  
+  
+  def fake_response 
+    # gets the input but we are building bones now 
+     fake_response = "The Great Gatsby is available and costs $19."
   end 
   
   def start 
