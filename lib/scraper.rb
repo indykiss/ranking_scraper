703fa2f661980b47ea6d673753a56ff42dcaa6f5
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-11-13 17:44:28 +0000

    separating classes

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,12 @@
 require_relative './config/environment.rb'
 
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+require_relative "./scraper/version"
+require_relative './scraper/scraper'
+require_relative './scraper/cli'
 
 module ScraperModule
   #code here
