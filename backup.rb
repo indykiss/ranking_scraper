6b79336db11b56239204d71a3083f26a3964298f
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-11-17 15:59:39 +0000

    cleaning up code

--- backup.rb
+++ backup.rb
@@ -144,5 +144,86 @@ end
   
   
   
+  #Random bits of code of previous attempts to do things:
+  # Under scraper/scraper:
+  #
+  # # def self.scraping_page
+ #   doc = Nokogiri::HTML(open("https://www.amazon.com/s/ref=lp_17296237011_pg_2?srs=17296237011&rh=i%3Aspecialty-aps&page=2&ie=UTF8&qid=1542059581"))
+    
+    # here I need to make this scrape the 3 books on the page 
+    
+ #   book = self.new 
+ #   book.title = doc.search("#result_16 h2").text
+ #   book.price = doc.search("#result_16 span.a-offscreen").text
+ #   book.availability = true
+ #   book 
+ # end 
+ 
+ 
+  #def self.connect_page 
+  #  url = HTTParty.get("http://books.toscrape.com/")
+  #  parsed_page = Nokogiri::HTML(url)
+   
+  #   parsed_page.css("div.container-fluid.page").collect do |attribute|
+      # could be div.col.sm-8.col-md-9
+  
+   #   book_list = {
+    #    :name => attribute.css("h3").text.strip,
+     #   :product_price => attribute.css("div.product_price").text.strip,
+    #    :availability => attribute.css("p.instock.availability").text.strip
+    #  }
+#  end 
+#end 
+#end 
+
+
+
+  #def data_to_info
+  # scrape_site.each do |rank|
+  #    RankingScraper::Scraper.new_from_index_page(rank)    
+  #  end 
+  #end 
+  
+
+# Bits of Random code under scraper/CLI
+
+
+# Keep getting an open uri error so doing HTTparty instead. Below code is working ok.
+# doc = Nokogiri::HTML(open("https://www.amazon.com/s/ref=lp_17296237011_pg_2?srs=17296237011&rh=i%3Aspecialty-aps&page=2&ie=UTF8&qid=1542059581")) 
+   
+   
+   
+   #example of what worked before amazon decided to be mean to me  
+   # book1 = self.new 
+  #  book_title1 = doc.search("#result_1 h2").text
+  #  book_price1 = doc.search("#result_1 span.a-offscreen").text
+
+    #@@all << [book_title1, book_price1] 
+    
+    
+
+
+# For .scraping_page
+  # ok so the item container actually returns the html for all the thing so that's actually fine 
+  # the below code is an option if I dont want to use the above code 3 times but doesn't work somehow... 
+  # doc.css("div.s-item-container").collect do |book| 
+  #    all_books = {
+  #    :title => book.css("h2").text, 
+  #    :price => book.css("span.a-offscreen").text}
+  #  @@all << all_books 
+  #  binding.pry 
+  
+# Some CSS for a different site which scrapped almost ok... meh 
+  #  My CSS Classes for the following string, BUT can't get the right info so trying amazon instead.
+  #  The given text are long strings which I don't want and I can't figure out how to narrow down.
+  #  doc = Nokogiri::HTML(open("https://www.esquire.com/entertainment/books/g14465218/best-books-of-2018/"))
+  #  title = doc.search("span.listicle-slide-hed-text").text 
+  #  prod_price = doc.search("span.product-slide-price").text 
+  #  descr = doc.search("div.slideshow-slide-dek").text 
+  
+  
+
+ 
+  #
 
   
--- lib/scraper/scraper.rb
+++ lib/scraper/scraper.rb
@@ -11,43 +11,5 @@ class ScraperModule::Scraper
     books 
   end 
 
- # def self.scraping_page
- #   doc = Nokogiri::HTML(open("https://www.amazon.com/s/ref=lp_17296237011_pg_2?srs=17296237011&rh=i%3Aspecialty-aps&page=2&ie=UTF8&qid=1542059581"))
-    
-    # here I need to make this scrape the 3 books on the page 
-    
- #   book = self.new 
- #   book.title = doc.search("#result_16 h2").text
- #   book.price = doc.search("#result_16 span.a-offscreen").text
- #   book.availability = true
- #   book 
- # end 
-
 end 
-
-
-  #def self.connect_page 
-  #  url = HTTParty.get("http://books.toscrape.com/")
-  #  parsed_page = Nokogiri::HTML(url)
-   
-  #   parsed_page.css("div.container-fluid.page").collect do |attribute|
-      # could be div.col.sm-8.col-md-9
-  
-   #   book_list = {
-    #    :name => attribute.css("h3").text.strip,
-     #   :product_price => attribute.css("div.product_price").text.strip,
-    #    :availability => attribute.css("p.instock.availability").text.strip
-    #  }
-#  end 
-#end 
-#end 
-
-
-
-  #def data_to_info
-  # scrape_site.each do |rank|
-  #    RankingScraper::Scraper.new_from_index_page(rank)    
-  #  end 
-  #end 
-  
 
