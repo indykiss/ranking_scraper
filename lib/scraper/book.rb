d2bd8cc52a133f7ec3f597170eced3c4f4ebd5cb
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-11-17 17:39:28 +0000

    saving

--- lib/scraper/book.rb
+++ lib/scraper/book.rb
@@ -2,26 +2,35 @@
 
 
 =begin 
-class ScraperModule::scraping_pageBook 
-  attr_accessor :title, :descr, :index
-  @@titles = []
+class ScraperModule::Book 
+  attr_accessor :title, :descr, :index, :book_object  
   @@all = []
+  @@titles = []
+  
+  def self.all
+    @@all
+  end
 
-  def initialize(title = nil, descr = nil)
-    @title = title 
-    @descr = descr 
-    @@titles << self.title
-    @@all << self 
+  #def initialize(title = nil, descr = nil)
+  #  @title = title 
+  #  @descr = descr 
+  #  @@titles << self.title
+  #  @@all << self 
+  #end 
+  
+  def book_object
+    @book_object ||= ScraperModule::Scraper.scraping_page 
   end 
 
-  def self.new_fromHTML(doc) 
-    doc = CLI.scraping_page
-    self.new = [index, title, descr]
-      index = doc.css("h4")[11].text
-      title = doc.css("h4")[0].text.gsub(/\s+/, ' ').strip
-      descr = doc.css("div.media-body")[0].text.strip
-    binding.pry
-  end 
+  #def self.new_fromHTML(doc) 
+   # doc = CLI.scraping_page
+    #self.new = [index, title, descr]
+    #  index = doc.css("h4")[11].text
+    #  title = doc.css("h4")[0].text.gsub(/\s+/, ' ').strip
+    #  descr = doc.css("div.media-body")[0].text.strip
+    #binding.pry
+  #end 
+
 
 end 
 
