63510d35d257ab390df095d2243fa08fb56f45ec
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-11-17 15:31:52 +0000

    SHE WORKS A LITTLE BETTER

--- lib/scraper/cli.rb
+++ lib/scraper/cli.rb
@@ -36,7 +36,7 @@ class ScraperModule::CLI
     # this gives me the index + title + author for simplicity 
     title1 = doc.search("h4")[0].text.gsub(/\s+/, ' ').strip
     # this gives me the beginning part of the book description 
-    descr1 = doc.search("div.media-body")[0].text
+    descr1 = doc.search("div.media-body")[0].text.strip 
 
     book1 = [index1, title1, descr1]
     
@@ -44,12 +44,12 @@ class ScraperModule::CLI
     #book2 = self.new
     index2 = doc.search("h4")[11].text
     title2 = doc.search("h4")[1].text.gsub(/\s+/, ' ').strip
-    descr2 = doc.search("div.media-body")[1].text
+    descr2 = doc.search("div.media-body")[1].text.strip 
     book2 = [index2, title2, descr2]
     
     index3 = doc.search("h4")[11].text
     title3 = doc.search("h4")[2].text.gsub(/\s+/, ' ').strip 
-    descr3 = doc.search("div.media-body")[2].text
+    descr3 = doc.search("div.media-body")[2].text.strip 
     book3 = [index3, title3, descr3]
     
 
@@ -74,11 +74,17 @@ class ScraperModule::CLI
       input = gets.strip
 
         if input == @@titles[0]
-          puts "#{@@titles[0]} is available and is about: #{@@descr[0]}."
+          puts "#{@@titles[0]} is about... #{@@descr[0]}."
+          puts "Here is the list of books available again." 
+          puts @@titles
         else if input == @@titles[1]
-          puts "#{@@titles[1]} is available and is about: #{@@descr[1]}."
+          puts "#{@@titles[1]} is about... #{@@descr[1]}."
+          puts "Here is the list of books available again." 
+          puts @@titles
         else if input == @@titles[2]
-          puts "#{@@titles[2]} is available and is about: #{@@descr[2]}."
+          puts "#{@@titles[2]} is about... #{@@descr[2]}."
+          puts "Here is the list of books available again." 
+          puts @@titles
           
          # binding.pry 
         else 
