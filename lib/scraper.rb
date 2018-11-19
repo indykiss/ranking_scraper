da6f66d5809e4602721cabede18d4d0e4ab2efda
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-11-19 17:54:52 +0000

    broke something...

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,11 +2,7 @@ require "open-uri"
 require "nokogiri"
 require "pry"
 
-require_relative "../lib/config/environment"
-require_relative "../lib/scraper/version"
-require_relative "../lib/scraper/scraper"
-require_relative "../lib/scraper/cli"
-require_relative "../lib/scraper/book"
+require_relative "./lib/config/environment"
 
 module ScrapModule
   #code here
