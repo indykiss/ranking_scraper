2c395c240dadd946c03067580092adaf2c43549b
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-11-24 19:54:18 +0000

    getting things working

--- lib/scraper/book.rb
+++ lib/scraper/book.rb
@@ -4,25 +4,46 @@ class Book
   attr_accessor :title, :descr, :index  
     @@titles = []
     @@descr = []
+    @titles = []
+    @descr = []
   
   def initialize(title=nil, descr=nil)
     @title = title
     @descr = descr
   end
   
-  def self.all
+  def making_book_object(book)
+      self.new(
+        book_info = {
+        :title => book.css("h4").text.gsub(/\s+/, ' ').strip,
+        :descr => book.css("div.media-body").text.strip,
+      }
+        )
+      puts book_info[:title] 
+
+      @titles << book_info.to_a[0][1].split(/[0-9]+[' ']+["."]+/)
+      
+      ugly_descr = book_info.to_a[1][1].split(/\n/)
+      
+      ugly_descr.each do |item|
+        if item.length > 40
+          @descr << item 
+        end 
+      end 
+  end 
+  end 
+
+  def self.titles 
+    puts @titles
+  end 
+  
+    def self.descr
+    puts @descr
+  end 
+
+  def self.all 
     @@all 
   end 
-  
-  def self.reset_all 
-    @@all.clear 
-  end 
-  
-  def self.titles 
-    @@titles
-  end 
-
-
 
 end 
 
--- lib/scraper/scraper.rb
+++ lib/scraper/scraper.rb
@@ -5,44 +5,21 @@ class Scraper
   @descr = [] 
   @all = []
 
+  def get_page 
+   url = HTTParty.get("https://thegreatestbooks.org/") 
+   doc = Nokogiri::HTML(url)    
+  end 
+
+  def access_container 
+    self.get_page.css("div.col-sm-7")
+  end 
+
   
   def self.scraping_page
-   url = HTTParty.get("https://thegreatestbooks.org/") 
-   doc = Nokogiri::HTML(url)     
-    
-    doc.css("div.col-sm-7").collect do |book|
-      book_info = {
-        :title => book.css("h4").text.gsub(/\s+/, ' ').strip,
-        :descr => book.css("div.media-body").text.strip,
-      }
-
-      puts book_info[:title] 
-
-      @titles << book_info.to_a[0][1].split(/[0-9]+[' ']+["."]+/)
-      
-      ugly_descr = book_info.to_a[1][1].split(/\n/)
-      
-      ugly_descr.each do |item|
-        if item.length > 40
-          @descr << item 
-        end 
-      end 
+    access_container.collect do |book| 
+    Book.making_book_object(book)
+    end 
   end 
-end 
-
-  def self.titles 
-    puts @titles
-  end 
-  
-  def self.descr
-    puts @descr
-  end 
-  
-  
-  def self.all 
-    @@all 
-  end 
-
 
   def self.answer 
     scraping_page
