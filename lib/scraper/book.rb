362a05e7f19dba6e1b6ae6054d0513cd8523be5f
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-11-24 15:10:25 +0000

    save

--- lib/scraper/book.rb
+++ lib/scraper/book.rb
@@ -2,15 +2,12 @@
 
 class Book 
   attr_accessor :title, :descr, :index  
-    @@all = []
     @@titles = []
     @@descr = []
   
-  def initialize(title=nil, descr=nil, index = nil)
+  def initialize(title=nil, descr=nil)
     @title = title
     @descr = descr
-    @index = index 
-    @@all << self
   end
   
   def self.all
@@ -25,14 +22,6 @@ class Book
     @@titles
   end 
 
-  #def self.new_fromHTML(doc) 
-   # doc = CLI.scraping_page
-    #self.new = [index, title, descr]
-    #  index = doc.css("h4")[11].text
-    #  title = doc.css("h4")[0].text.gsub(/\s+/, ' ').strip
-    #  descr = doc.css("div.media-body")[0].text.strip
-    #binding.pry
-  #end 
 
 
 end 
