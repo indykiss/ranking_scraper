38cd45fc002724c76d2e2a486171a8edb8b81c90
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-12-20 17:48:35 +0000

    done!

--- lib/scraper/cli.rb
+++ lib/scraper/cli.rb
@@ -27,11 +27,7 @@ class ScraperModule::CLI
       puts "Please type book number you'd like to know more about."
       input = gets.strip
 
-      if input.match(/A-Z/)
-        puts "That's not a number between 1 - 50. Please try again."
-        return 
-        
-        elsif input.to_i-1 <= ScraperModule::Book.all.size
+        if input.to_i-1 <= ScraperModule::Book.all.size
           book = ScraperModule::Book.all[input.to_i-1]
           puts "That book is #{book.title}"
           puts "This book is about... #{book.descr}"
