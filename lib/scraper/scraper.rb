15a49455dbb5add245f4f250a1e52d0a7d863b71
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-01-11 19:32:30 +0000

    refactored an if statement in scraper.rb to clean up book.rb

--- lib/scraper/scraper.rb
+++ lib/scraper/scraper.rb
@@ -7,11 +7,14 @@ class ScraperModule::Scraper
    
    get_page = doc.search("div.col-xs-12")
     
-    get_page.each do |book|
-      new_book = ScraperModule::Book.new 
+  
+  get_page.each do |book|
+      if book.search("h4").text.gsub(/[^0-9]/, '') != ""
+        new_book = ScraperModule::Book.new 
         new_book.index = book.search("h4").text.gsub(/[^0-9]/, '')
         new_book.title = book.search("h4 a").text.gsub(/\s+/, ' ')
         new_book.descr = book.search("div p").children.text.strip
-    end 
+      end 
+    end   
   end 
 end 
